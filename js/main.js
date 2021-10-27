152a6b1e983e00e182120f118a613222c7932a1b
Luckbox314 <lucas.muoz@uc.cl>
2021-10-27 17:04:20 -0300

    feat: harbor arrival animation

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var setup = true;
 
 $(document).ready(function(){
     $('html, body').scrollTop(0);
-
+    
     $(window).on('load', function() {
         setTimeout(function(){
             $('html, body').scrollTop(0);
@@ -19,7 +19,13 @@ $(document).ready(function(){
     height = $(window).height();
     width = $(window).width();
     boatHeight = $("#boat").height();
+
+    $("body").css("height", $("main").height() + height/2 + boatHeight/2);
+
     console.log($("main").height());
+
+
+
     console.log(height);
     console.log(boatHeight);
     console.log($("main").height() - (height/2 + boatHeight/2));
@@ -47,27 +53,38 @@ function harbor() {
     }
     
     $("#odometer").text(scroll);
-    if (scroll > height/2 - boatHeight/2) { // not in harbor
-        if (inHarbor) { // if get out of harbor
-            inHarbor = !inHarbor;
+    if (scroll > height/2 - boatHeight/2 && scroll < 11000 - height/2 - boatHeight/2) { // not in harbor
+        if (inHarbor || inFinalHarbor) { // if get out of harbor
+            inHarbor = false;
+            inFinalHarbor = false;
             $("main").css("position", "static");
             $("main").css("margin-top", `${height/2 - boatHeight/2}px`); 
             $("#section").text('Open Ocean');
         }
     }
 
-    // else if (scroll >= $("main").height() - (height/2 - boatHeight/2)) {
-    //     $("#section").text('End Harbor');
-    // }
+    else if (scroll >= 11000 - height/2 - boatHeight/2) {
+        if (!inFinalHarbor) {
+            inFinalHarbor = true;
+            
+            $("main").css("position", "fixed");
+            $("#section").text('Final Harbor');
+            $("main").css("top", `${-(height/2 - boatHeight/2) - $("main").height() + height}px` );
+            
+        }
+        $("#boat").css("top", `${scroll - 11000 + height}px`);
+         
+    }
 
     else { // in harbor
         if (!inHarbor) { // if get in harbor
-            inHarbor = !inHarbor;
+            inHarbor = true;
             $("main").css("margin-top", `0px`);
             $("main").css("position", "fixed");
+            $("main").css("top", `0px`);
             $("#section").text('Harbor');
         }
         $("#boat").css("top", `${scroll}px`);
-        $("main").css("top", `0px`);  
+          
     }
 }
